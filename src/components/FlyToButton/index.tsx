a7036d970a066ffa0ff246fd14d230f6d151d1d0
Layr <layrfpf@gmail.com>
2024-05-31 15:55:27 -0300

    feat: fly to (more dynamic inputs)

--- src/components/FlyToButton/index.tsx
+++ src/components/FlyToButton/index.tsx
@@ -2,14 +2,20 @@ import { useGlobusContext } from '@openglobus/openglobus-react'
 import './styles.css'
 import { LonLat } from "@openglobus/og";
 
-export default function FlyToButton() {
+interface FlyToButtonProps {
+    lat: string | undefined,
+    lon: string | undefined
+}
+
+export default function FlyToButton({ lat, lon }: FlyToButtonProps) {
     const { globus } = useGlobusContext()
 
     const clickFlyTo = () => {
         let ell = globus?.planet.ellipsoid;
 
-        let destPos = new LonLat(-43.2104872, -22.951916, 5000);
-        let viewPoi = new LonLat(-43.2104872, -22.951916, 0);
+        let destPos = new LonLat(Number(lon), Number(lat), 5000);
+        let viewPoi = new LonLat(Number(lon), Number(lat), 0);
+        
         if (ell) {
             let lookCart = ell.lonLatToCartesian(viewPoi);
             let upVec = ell.lonLatToCartesian(destPos).normalize();
@@ -18,7 +24,7 @@ export default function FlyToButton() {
         }
     }
 
-    return <div className={'button-container'}>
-        <button onClick={clickFlyTo}>Voar para o Cristo Redentor</button>
-    </div>
+    return (
+        <button id={"fly-to-btn"} onClick={clickFlyTo}>Voar para destino</button>
+    )
 }
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import FlyToButton from "../FlyToButton";
 import "./styles.css";
 
 export default function Menu() {
 
+    const [ lat, setLat ] = useState<string | undefined>(undefined)
+    const [ lon, setLon ] = useState<string | undefined>(undefined)
+
+    const autoFillLatLon = () => {
+        setLat("-22.951916")
+        setLon("-43.2104872")
+    }
+
     return(
         <div className={"menu-container"}>
             <div className={"inner-container"}>
-                <FlyToButton />
+                <input className={"fly-to-coords-input"} placeholder="Latitude" value={lat} onChange={e => setLat(e.target.value)} />
+                <input className={"fly-to-coords-input"} placeholder="Longitude" value={lon} onChange={e => setLon(e.target.value)} />
+                <button id={"temporary-btn"} onClick={autoFillLatLon}>Preencher automaticamente</button>
+                <FlyToButton lat={lat} lon={lon} />
             </div>
         </div>
     )
